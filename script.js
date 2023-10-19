14047a1d1b968b36bc9e63d68815f41071467f9e
mrAbhiraj <129583565+mrAbhiraj@users.noreply.github.com>
2023-10-19 15:23:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var signup = document.querySelector(".d-h-acc h2 a") ;
+var spage = document.querySelector(" #right-s #all-content")
+var lpage = document.querySelector(" #right-l #all-content")
+signup.addEventListener("click" , function(){
+   spage.classList.toggle = "active" ;
+    lpage.style.display = "none"
+})
